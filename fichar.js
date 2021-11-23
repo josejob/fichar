7415d4fd2028cde7e15f6ca456bf163d4ebeab7d
jose_fhios <jose.martinez@fhios.es>
2021-11-23 01:44:57 +0100

    refactor(console): make userData more readeable

--- fichar.js
+++ fichar.js
@@ -2,27 +2,15 @@
 const { chromium } = require('playwright')
 const fs = require('fs')
 const fileUserCredentials = './user.json'
-const defaultUsername = '11111111A'
-const defaultPassword = '1111'
-const defaultLongitude = 41.111111
-const defaultlatitude = 2.111111
-let userName = ''
-let userPasword = ''
-let userLongitude = ''
-let userLatitude = ''
+const userData = { username: '11111111A', password: '1111', longitude: 41.111111, latitude: 2.111111 }
 
 try {
   if (fs.existsSync(fileUserCredentials)) {
     const obj = JSON.parse(fs.readFileSync(fileUserCredentials, 'utf8'))
-    userName = obj.username
-    userPasword = obj.password
-    userLongitude = obj.longitude
-    userLatitude = obj.latitude
-  } else {
-    userName = defaultUsername
-    userPasword = defaultPassword
-    userLongitude = defaultLongitude
-    userLatitude = defaultlatitude
+    userData.username = obj.username
+    userData.password = obj.password
+    userData.latitude = obj.latitude
+    userData.longitude = obj.longitude
   }
 } catch (err) {
   console.log(err)
@@ -32,14 +20,14 @@ try {
   const browser = await chromium.launch({ headless: true })
 
   const context = await browser.newContext({
-    geolocation: { longitude: userLongitude, latitude: userLatitude },
+    geolocation: { longitude: userData.longitude, latitude: userData.latitude },
     permissions: ['geolocation']
   })
   const page = await context.newPage()
   await page.goto('https://a3gt.wolterskluwer.es/gt#/clockings/57962')
 
-  await page.type('#username', userName)
-  await page.type('#pwd', userPasword)
+  await page.type('#username', userData.username)
+  await page.type('#pwd', userData.password)
 
   await page.click('#sendClocking')
   await page.waitForSelector('div.notice')
