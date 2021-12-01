f489aac092be4fb8d11b8bb62c466159c16c83f9
jose_fhios <jose.martinez@fhios.es>
2021-12-01 18:22:46 +0100

    feature(console): add tracelog
    active or disable tracelog in user.json file

--- fichar.js
+++ fichar.js
@@ -3,6 +3,7 @@ const { chromium } = require('playwright')
 const fs = require('fs')
 const fileUserCredentials = './user.json'
 const userData = { username: '11111111A', password: '1111', longitude: 41.111111, latitude: 2.111111 }
+let activeTraceLog = false
 
 try {
   if (fs.existsSync(fileUserCredentials)) {
@@ -11,13 +12,14 @@ try {
     userData.password = obj.password
     userData.latitude = obj.latitude
     userData.longitude = obj.longitude
+    activeTraceLog = obj.tracelog
   }
 } catch (err) {
   console.log(err)
 }
 
 (async () => {
-  const browser = await chromium.launch({ headless: true })
+  const browser = await chromium.launch({ headless: false })
 
   const context = await browser.newContext({
     geolocation: { longitude: userData.longitude, latitude: userData.latitude },
@@ -27,16 +29,19 @@ try {
       height: 937
     }
   })
+  if (activeTraceLog) await context.tracing.start({ screenshots: true, snapshots: true })
   const page = await context.newPage()
+
   await page.goto('https://a3gt.wolterskluwer.es/gt#/clockings/57962')
 
   await page.type('#username', userData.username)
   await page.type('#pwd', userData.password)
 
-  await page.click('#sendClocking')
-  await page.waitForSelector('div.notice')
+  // await page.click('#sendClocking')
+  // await page.waitForSelector('div.notice')
 
   await page.screenshot({ path: './screenshots/' + 'fichar' + '.png' })
+  if (activeTraceLog) await context.tracing.stop({ path: './tracelog/trace.zip' })
 
   await page.close()
   await browser.close()
